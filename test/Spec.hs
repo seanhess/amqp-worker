877e0a74c80ea1c9e224d13301b9a162569e26a7
Sean Hess <seanhess@gmail.com>
2016-10-25 15:30:00 -0600

    stubbed in tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,4 +1,16 @@
 module Main where
 
+import Test.Tasty
+import Test.Tasty.HUnit
+
 main :: IO ()
-main = putStrLn "TESTS"
+main = do
+  defaultMain tests
+
+
+tests :: TestTree
+tests = testGroup "Tests"
+  [ testCase "TODO" testExample ]
+
+testExample :: Assertion
+testExample = pure ()
