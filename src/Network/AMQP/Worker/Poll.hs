03f093ac734acf82921587449902449e8aa9555d
Sean Hess <seanhess@gmail.com>
2017-01-03 12:11:17 -0700

    0.2.4 fixed memory leak in poll

--- src/Network/AMQP/Worker/Poll.hs
+++ src/Network/AMQP/Worker/Poll.hs
@@ -5,12 +5,13 @@ module Network.AMQP.Worker.Poll where
 import Control.Concurrent (threadDelay)
 import Control.Monad.Trans.Control (MonadBaseControl)
 import Control.Monad.Base (liftBase)
+import Control.Monad.Loops (untilJust)
 
 poll :: (MonadBaseControl IO m) => Int -> m (Maybe a) -> m a
-poll us action = do
+poll us action = untilJust $ do
     ma <- action
     case ma of
-      Just a -> return a
+      Just a -> return $ Just a
       Nothing -> do
         liftBase $ threadDelay us
-        poll us action
+        return Nothing
